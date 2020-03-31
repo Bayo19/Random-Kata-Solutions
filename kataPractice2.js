28a3a9a56b4f8fd05ea9b558a07fd1c25ffc0f8c
Bayo Ade <bayo.300@gmail.com>
2020-03-31 10:20:17 +0100

    another one

--- kataPractice2.js
+++ kataPractice2.js
@@ -967,12 +967,3 @@ console.log(spinWords("Just kidding there is still one more"))
 console.log(spinWords('Seriously this is the last one'))
 
 console.clear()
-
-function removeSmallest(numbers) {
-    let x = new Set(numbers)
-
-    return x
-}
-
-console.log(removeSmallest([5, 3, 2, 1, 4]))
-console.log(removeSmallest([2, 2, 1, 2, 1]))
