c93796f161c25734ec7c555efd621fcd0d6e69bb
Bayo Ade <bayo.300@gmail.com>
2020-03-31 18:32:49 +0100

    .

---
+++ convert_primitive.js
@@ -0,0 +1,3 @@
+// when objects are added together they are auto-converted to primitives
+
+// Date objects can be subtracted; the result of date1 - date2 is the time difference between the two dates
