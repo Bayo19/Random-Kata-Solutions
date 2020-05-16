a0f8055486538c6b48928144e56a63bb6b7621a9
Bayo Ade <bayo.300@gmail.com>
2020-05-16 13:58:41 +0100

    function to get a color between two other colors

--- myRandomShit.js
+++ myRandomShit.js
@@ -45,3 +45,37 @@ function formParser(email, password, validate) {
 
 console.log(formParser('john@gmail.com', '57666', '57666'))
 console.log(formParser('stacyanne@hotmail.com', '34546', '2352454'))
+console.clear()
+
+let rando = () => {
+    let r,g,b
+    r = Math.floor(Math.random() * 256)
+    g = Math.floor(Math.random() * 256)
+    b = Math.floor(Math.random() * 256)
+
+    return `rgb(${r},${g}, ${b})`
+}
+
+// new color between two colors
+
+let getNewColor = (a,c) => {
+    let a1 = a.replace(/[a-z\(\)]/g,'').split(',')
+    let c1 = c.replace(/[a-z\(\)]/g,'').split(',')
+
+    let a2 = a1.map(function(x){
+        return Number(x)
+    })
+    let c2 = c1.map(function(x){
+        return Number(x)
+    })
+    
+   
+    let r = Math.floor((a2[0]+c2[0]) /2)
+    let g = Math.floor((a2[1] + c2[1]) /2)
+    let b = Math.floor((a2[2] + c2[2]) /2)
+
+   res = `rgb(${r},${g},${b})`
+    return res
+}
+
+console.log(getNewColor(rando(),  rando()))
